3e1335eda439bdc3e6741765fb2896f18d6ae9cf
agwhite2004 <agw00027@mix.wvu.edu>
2024-02-22 12:34:29 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+let section_number = "002";
+let wvu_id = "800405096";
+let name = "Alex";
+
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - {section_number}</h2>
+      <p>{wvu_id}</p>
+      <p>Hi I am {name}</p>
+    </div>
+  );
+}
+
+export default App;
