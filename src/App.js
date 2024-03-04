c3ac38bd6c9991ef3421975508b06959c26a908c
agwhite2004 <agw00027@mix.wvu.edu>
2024-03-03 22:13:10 -0500

    Test for Event and Card

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ let name = "Alex";
 
   return (
     <div className="App">
-
+      {Navbar()}
       <h1>CS 230L</h1>
       <h2>Section - {section_number}</h2>
       <p>{wvu_id}</p>
       <p>Hi I am {name}</p>
+      {Card("Test", "This is a test")}
     </div>
   );
 }
